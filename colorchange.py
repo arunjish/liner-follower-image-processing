02dfd6184cfa8ea57ccbe7165a5f6fd3f1fab7d9
arunjishnu-aarduin <lettertojishnu@gmail.com>
2017-04-20 17:33:07 +0000

    steper final

--- colorchange.py
+++ colorchange.py
@@ -5,7 +5,7 @@ import time
 import cv2 
 import numpy as np
 
-from motorControl import *
+from step import *
 import sys
 
 fdelay=0
@@ -15,12 +15,16 @@ turn = False
 turnDir="k"
 turn_bottom_edge_count=0
 turnLoop=False
+turnOver=False
 np.set_printoptions(threshold=np.nan)
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = (250,250)
+#camera.resolution = (250,250)
+camera.resolution = (640,480)
+
 camera.framerate = 14
-rawCapture = PiRGBArray(camera, size=(250, 250))
+#rawCapture = PiRGBArray(camera, size=(250, 250))
+rawCapture = PiRGBArray(camera, size=(640,480))
 # allow the camera to warmup
 time.sleep(1)
 count=0
@@ -29,117 +33,148 @@ initial_skip=5
 skip_frame_count=0
 direction="k"
 
-
+mid1corrected=False
+bottom1corrected=False
+mid2corrected=False
 def checkFrame(edgex):
+	h,w=edgex.shape
+	h=h-100
       	m_y1=0
        	m_y2=0
         mid_edge_count=0
+	mid_centre=0	
 	top_l_edge_count=0
 	top_r_edge_count=0
 	top_line_edge_count=0
+	top_line_centre=0
 	top_r_centre=0
 	top_l_centre=0
 	bottom_line_edge_count=0
-        for i in range(0,240):
-                if edgex[125][i]==255:
+	bottom_line_centre=0
+        for i in range(0,h-10):
+                if edgex[h/2][i]==255:
                         mid_edge_count+=1
                         if mid_edge_count==1:
-                               m_y1=i
+                               mid_centre=i
                         elif mid_edge_count==2:
-                               m_y2=i
-		if edgex[i+10][10]==255:
+                                mid_centre=int((mid_centre+i)/2)
+			
+
+
+
+		if edgex[i+10][40]==255:
                         top_l_edge_count+=1
 			if top_l_edge_count==1:
                                top_l_centre=i
                         elif top_l_edge_count==2:
                 		top_l_centre=int((top_l_centre+i)/2)
-		if edgex[i+10][240]==255:
+			
+
+		if edgex[i+10][w-40]==255:
                         top_r_edge_count+=1
 			if top_r_edge_count==1:
                                top_r_centre=i
                         elif top_r_edge_count==2:
                                 top_r_centre=int((top_r_centre+i)/2)
+			
 
-		if edgex[0][i]==255:
+		if edgex[30][i]==255:
 			top_line_edge_count+=1
-		if edgex[249][i]==255:
-                        bottom_line_edge_count+=1
+			if top_line_edge_count==1:
+                               top_line_centre=i
+                        elif top_line_edge_count==2:
+                                top_line_centre=int((top_line_centre+i)/2)
+                        
 
-	mid_centre=int((m_y1+m_y2)/2)
-	return ((top_l_edge_count,top_l_centre),(mid_edge_count,mid_centre),(top_r_edge_count,top_r_centre),top_line_edge_count,bottom_line_edge_count)
+
+		if edgex[h-30][i]==255:
+                        bottom_line_edge_count+=1
+			if bottom_line_edge_count==1:
+                               bottom_line_centre=i
+                        elif bottom_line_edge_count==2:
+                                bottom_line_centre=int((bottom_line_centre+i)/2)
+				
+			
+	
+	return ((top_l_edge_count,top_l_centre),(mid_edge_count,mid_centre),(top_r_edge_count,top_r_centre),(top_line_edge_count,top_line_centre),(bottom_line_edge_count,bottom_line_centre))
 
 
 def directBot(frame_res):
-	top_l,mid,top_r,top_line_edge_count,bottom_line_edge_count=frame_res
+	top_l,mid,top_r,top_line,bottom_line=frame_res
 	mid_edge_count,mid_centre=mid
 	top_l_edge_count,top_l_centre=top_l
 	top_r_edge_count,top_r_centre=top_r
+	top_line_edge_count,top_line_centre=top_line
+	bottom_line_edge_count,bottom_line_centre=bottom_line
 	global turn
 	global turnDir
 	global turn_bottom_edge_count
-	#if  not 0<mid_edge_count<=2:
-	#	print " previous"
+	global mid1corrected
+	global bottom1corrected
+	global mid2corrected
 
-	#	stop(0)
+	
 	if top_l_edge_count==2 and top_r_edge_count==2 and top_line_edge_count==2 :
 		print "plus junction"
-		forward(fdelay)
-                #stop(2)
-                diffRight(tdelay)
-                stop(0)
-
-		stop(2)
+		#forward(fdelay)
+               
+               # diffRight(tdelay)
 	elif top_l_edge_count==2 and top_line_edge_count==2 and top_r_edge_count==0:
                 print "left T junction"
-		forward(fdelay)
-                #stop(3)
-                diffLeft(tdelay)
-                stop(0)
+		#forward(fdelay)
+                #diffLeft(tdelay)
+               
 
-                stop(2)
+                
 	elif top_r_edge_count==2 and top_line_edge_count==2 and top_l_edge_count==0 :
                 print "right T junction"
-		forward(fdelay)
+		#forward(fdelay)
                 #stop(2)
 
 	elif top_l_edge_count==2 and top_line_edge_count==2 and top_r_edge_count==2:
 		print "T junction"
-		forward(fdelay)
-                #stop(2)
-                diffLeft(tdelay)
-                stop(0)
-
+		#forward(fdelay)
+                #diffLeft(tdelay)
+               
 	elif top_l_edge_count==2 and top_r_edge_count==0  :
-		print "left turn Ahead"
+		print "\t\t\tleft turn Ahead"
 		turn=True
 		turnDir="l"
-		turn_bottom_edge_count=0
-		forward(0)
-	elif top_l_edge_count ==0 and  top_r_edge_count==2:
-		print "right turn detected"
-		forward(fdelay)
-                #stop(2)
-                diffRight(tdelay)
-                turn=True
+		mid1corrected=False
+		bottom1corrected=False
+		mid2corrected=False
 
-	elif 0<mid_centre<=83:
-		left(.2)
-		stop(0)
-	elif 83<mid_centre<=166:
-		forward(0)
-	elif 166<mid_centre<=249:
-		right(.2)
-		stop(0)
+		#exit(True)
+		#turn_bottom_edge_count=0
+		#forward(0)
+	elif top_l_edge_count ==0 and  top_r_edge_count==2 :
+		print "\t\t\tright turn detected"
+		turn=True
+                turnDir="r"
+		mid1corrected=False
+		bottom1corrected=False
+		mid2corrected=False
+
+		
+	elif 0<mid_centre<200:
+		left(5,0.001)		
+	elif 200<=mid_centre<=210:
+		forward(40,0.003)
+	elif 210<mid_centre<=400:
+		right(5,0.001)
+	
 		
 
 
 def printFrame(im_bw):
-	for x in range(0,250):
+	h,w=im_bw.shape
+	h=h-100
+	for x in range(0,h):
 		
 		row_count_black=0
 		row_count_white=0
 		sys.stdout.write(str(x)+ " : ")
-        	for y in range(0,250):
+        	for y in range(0,w):
                 	if im_bw[x][y] == 0 :
                         	sys.stdout.write("#")
 				row_count_black+=1
@@ -163,40 +198,171 @@ try:
 		#skip_frame_count=0
 		if skip_frame_count>0:
 			skip_frame_count-=1
-			print "skipp frame"
+			#print "skipp frame"
 			rawCapture.truncate(0)
 			continue	
 		
 		img_RGB = frame.array
+		img_RGB=cv2.resize(img_RGB,(400,400))
+		
 		edge=cv2.Canny(img_RGB,100,200)
 		
 		#printFrame(edge)
 		frame_res= checkFrame(edge)
-		print frame_res
-
-		top_l,mid,top_r,top_line_edge_count,bottom_line_edge_count=frame_res
+		print "\n\n\t\t Top-L | Mid | Top-R | Top-Line | Bottom Line  "
+		print "\t\t"+ str(frame_res)+"\n\n"
+		
+		top_l,mid,top_r,top_line,bottom_line=frame_res
 	        mid_edge_count,mid_centre=mid
 	        top_l_edge_count,top_l_centre=top_l
 	        top_r_edge_count,top_r_centre=top_r
+		top_line_edge_count,top_line_centre=top_line
+		bottom_line_edge_count,bottom_line_centre=bottom_line
+
+		if 200<=mid_centre<=210:
+        	        mid_ok=True
+	        else:
+                	mid_ok=False
+
+		if 200<=bottom_line_centre<=210:
+                        bottom_ok=True
+                else:
+                        bottom_ok=False
 
 		
+
 		if turn==True :
-			if bottom_line_edge_count ==0 :
-				turn_bottom_edge_count+=1
-				if turn_bottom_edge_count>8:
-					diffLeft(0)
-					turnLoop=True
-					turn=False
 			
+			if  bottom_ok:
+                                print "\t\t\t\t\tPre Turn Correction compleated"
+                                turn=False
+				turnLoop=True
+                                t2 = datetime.now()
+                                delta=t2-t1
+					
+				if turnDir=="l":
+                                	dist=int((270-top_l_centre)/2)   #bottom line centre - topline centre
+                                	forward(dist+40,0.005)
+
+                                
+	                        if turnDir=="r":
+                                	dist=int((270-top_r_centre)/2)
+                                	forward(dist,0.005)
+
+                              
+
+
+                                skip_frame_count=int(delta.microseconds/100000)+2
+			
+
+                                rawCapture.truncate(0)
+                                continue
+                        elif not bottom_ok :
+                                print "\t\t\t\tPre turn bottom  correcting...."
+
+                                if 0<bottom_line_centre<200:
+                                        diffLeft(5,0.001)
+
+                                elif 200<bottom_line_centre<=400:
+                                        diffRight(5,0.001)
+
 		if turnLoop==True:
-			print frame_res
-			if  top_l_edge_count>0 or top_r_edge_count>0:
-				stop(0)
-				print " Turn over"
+			#if  bottom_line_edge_count !=2:
+			print turnDir
+			if turnDir=="l":
+				 diffLeft(100,0.001)   #bottom line centre - topline centre
+                           
+			if turnDir=="r":
+                                diffRight(100,0.001)
+			if bottom_ok:
 				turnLoop=False
-				
-	
-		if turn==False and turnLoop==False:
+				turnOver=True
+				print "Turn over"
+			t2 = datetime.now()
+	                delta=t2-t1
+
+        	        skip_frame_count=int(delta.microseconds/100000)
+
+			rawCapture.truncate(0)
+                        continue
+		
+		if 200<=bottom_line_centre<=210:
+                        bottom_ok=True
+                else:
+                        bottom_ok=False
+
+
+		if turnOver==True:
+			
+			if  bottom_ok:
+                                print "\t\t\t\t\tPost Turn Correction compleated"
+                                turnOver=False
+                                t2 = datetime.now()
+                                delta=t2-t1
+
+                                skip_frame_count=int(delta.microseconds/100000)
+
+
+                                rawCapture.truncate(0)
+                                continue
+                        elif not bottom_ok :
+                                print "\t\t\t\tPre turn bottom  correcting...."
+
+                                if 0<bottom_line_centre<200:
+                                        left(10,0.001)
+
+                                elif 200<bottom_line_centre<=400:
+                                        right(10,0.001)
+
+			"""			
+			if  mid_ok and  mid2corrected:
+                                print "\t\t\t\t\tPost Turn Correction compleated"
+                                turnOver=False
+				t2 = datetime.now()
+		                delta=t2-t1
+
+		                skip_frame_count=int(delta.microseconds/100000)
+
+				rawCapture.truncate(0)
+	                        continue
+
+                        	
+                               
+			elif not mid_ok and not mid1corrected:
+				print "\t\t\t\tPost turn mid  correcting...."
+
+				if 0<mid_centre<200:
+                                        left(5,0.001)
+
+                                elif 210<mid_centre<=400:
+                                        right(5,0.001)
+			elif  mid_ok and not mid1corrected:
+				mid1corrected=True
+			elif not bottom_ok and not bottom1corrected:
+                                print "\t\t\t\tPost turn bottom  correcting...."
+
+                                if 0<bottom_line_centre<200:
+                                        diffLeft(5,0.001)
+
+                                elif 200<bottom_line_centre<=400:
+                                        diffRight(5,0.001)
+			elif bottom_ok and not bottom1corrected:
+				bottom1corrected=True
+
+			elif not mid_ok and bottom1corrected:
+				print "\t\t\t\tPost turn mid 2  correcting...."
+
+                                if 0<mid_centre<200:
+                                        diffLeft(5,0.001)
+
+                                elif 210<mid_centre<=400:
+                                        diffRight(5,0.001)
+			elif mid_ok and bottom1corrected:
+                                mid2corrected=True
+
+		
+			"""
+		if turn==False and turnLoop==False and turnOver==False:
 			directBot(frame_res)
 		t2 = datetime.now()
                 delta=t2-t1
@@ -212,10 +378,10 @@ try:
 		#processimage(image_RGB)
 	
 		#clear the stream in preparation for the next frame
+	
 		rawCapture.truncate(0)
 
 except KeyboardInterrupt:
 	print "Goodbye"
-	stop(0)
 	exit()
 
--- step.py
+++ step.py
@@ -15,130 +15,101 @@ GPIO.setup(MR1,GPIO.OUT)
 GPIO.setup(MR2,GPIO.OUT)
 
 
-def forward(num):
+def forward(step,step_delay):
 	GPIO.output(ML1, True)
 	GPIO.output(MR1, False)
+	time.sleep(step_delay)
 
-	for i in range(0,num*200):
+	for i in range(0,step):
         	GPIO.output(ML2, True)
         	GPIO.output(MR2, True)
 
-        	time.sleep(.004)
+        	time.sleep(step_delay)
         	GPIO.output(ML2, False)
         	GPIO.output(MR2, False)
 
-        	time.sleep(.004)
-        print "forward"
-forward(2)
-
-"""
-def backward(delay):
-	GPIO.output(ML2, True)
+        	time.sleep(step_delay)
+        print str(step)+"  steps  forward"
+def backward(step,step_delay):
         GPIO.output(ML1, False)
+        GPIO.output(MR1, True)
+	time.sleep(step_delay)
+	
+        for i in range(0,step):
+                GPIO.output(ML2, True)
+                GPIO.output(MR2, True)
 
-        GPIO.output(MR2, True)
-        GPIO.output(MR1, False)
+                time.sleep(step_delay)
+                GPIO.output(ML2, False)
+                GPIO.output(MR2, False)
 
-	time.sleep(delay)
-        print "Backwords"
+                time.sleep(step_delay)
+        print "backward"
 
-def left(delay):
+def diffRight(step,step_delay):
         GPIO.output(ML1, True)
-        GPIO.output(ML2, False)
-
-        GPIO.output(MR1, False)
-        GPIO.output(MR2, False)
-	time.sleep(delay)
-        print "left"
-
-def right(delay):
-        GPIO.output(ML1, False)
-        GPIO.output(ML2, False)
-
         GPIO.output(MR1, True)
-        GPIO.output(MR2, False)
+        time.sleep(step_delay)
+	print str(step) + " steps diff right"
 
-	time.sleep(delay)
-        print "right"
+        for i in range(0,step):
+                GPIO.output(ML2, True)
+                GPIO.output(MR2, True)
 
-def stop(delay):
+                time.sleep(step_delay)
+                GPIO.output(ML2, False)
+                GPIO.output(MR2, False)
+
+                time.sleep(step_delay)
+        
+def diffLeft(step,step_delay):
         GPIO.output(ML1, False)
-        GPIO.output(ML2, False)
-
         GPIO.output(MR1, False)
-        GPIO.output(MR2, False)
-        print "Stop"
-	time.sleep(delay)
+        time.sleep(step_delay)
+	print str(step) +" steps diff left"
+        for i in range(0,step):
+		
 
-def diffLeft(delay):
-	GPIO.output(ML1, True)
-        GPIO.output(ML2, False)
+                GPIO.output(ML2, True)
+                GPIO.output(MR2, True)
 
-        GPIO.output(MR1, False)
-        GPIO.output(MR2, True)
-        print "\t diff---left"
-	time.sleep(delay)
+                time.sleep(step_delay)
+                GPIO.output(ML2, False)
+                GPIO.output(MR2, False)
 
-def diffRight(delay):
+                time.sleep(step_delay)
+        
+def left(step,step_delay):
         GPIO.output(ML1, False)
-        GPIO.output(ML2, True)
+        GPIO.output(MR1, False)
+        time.sleep(step_delay)
 
-        GPIO.output(MR1, True)
-        GPIO.output(MR2, False)
-        print "\t diff---Right"
-        time.sleep(delay)
+        for i in range(0,step):
+               # GPIO.output(ML2, True)
+                GPIO.output(MR2, True)
 
+                time.sleep(step_delay)
+                #GPIO.output(ML2, False)
+                GPIO.output(MR2, False)
 
-def follow_previous_direction(direction):
-                        if direction=="r":
-                                right(0)
-                                time.sleep(0.05)
-                                stop()
-                        elif direction=="l":
-                                left(0)
-                                time.sleep(0.05)
-                                stop()
+                time.sleep(step_delay)
+        print str(step) +"step left"
 
+def right(step,step_delay):
+        GPIO.output(ML1, True)
+        GPIO.output(MR1, False)
+        time.sleep(step_delay)
 
+        for i in range(0,step):
+               	GPIO.output(ML2, True)
+                #GPIO.output(MR2, True)
 
-def directRobot(count_b_top,count_b_bottom,count_b_left,count_b_right,skipped_frame,direction):
+                time.sleep(step_delay)
+                GPIO.output(ML2, False)
+                #GPIO.output(MR2, False)
 
-                print "\n\n-------------------------------------------------------------------------------------------------------------\n"
-                print "\t\t\t top = "+ str(count_b_top)
-                print "left = "+str(count_b_left)
-                print "\t\t\t\t\t\tright =  "+ str(count_b_right)
-                print "\t\t\tbottom = "+str(count_b_bottom)
-
-                
-
-                if count_b_top > 70 and count_b_bottom > 70 and count_b_left < 70 and count_b_right < 70:
-                        forward(0)
-                        direcrion="f"
-                elif count_b_bottom>70 and count_b_right>70 and skipped_frame:
-                        right(0)
-                        direction="r"
-                elif  count_b_left>70 and not skipped_frame:
-                        left(0)
-                        direction="l"
-                        time.sleep(0.05)
-                        stop()
-                elif count_b_right>70 and not skipped_frame:
-                        right(0)
-                        direction="r"
-                        time.sleep(0.05)
-                        stop()
-                elif count_b_top < 70 and count_b_bottom < 70 and count_b_left < 70 and count_b_right < 70:
-                        print "full white"
-                        follow_previous_direction()
-
-                else :
-                        print "unkonwn condition"
-                        follow_previous_direction()
-                       # stop()
+                time.sleep(step_delay)
+        print str(step) +"step right"
 
 
-                print "\n\n-------------------------------------------------------------------------------------------------------------\n\n"
-
-                
-"""
 
